1531197be35886cfef0c9090a99678071b5c3b8d
coregetafe2020 <coregetafe2020@gmail.com>
2020-10-01 11:19:03 +0200

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ListadoProductosComponent } from './productos/listado-productos/listado-productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: InicioComponent},
+  {path: 'listado-productos', component: ListadoProductosComponent},
+  {path: 'listado-clientes', component: ListadoClientesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { NavComponent } from './comunes/nav/nav.component';
+import { ListadoProductosComponent } from './productos/listado-productos/listado-productos.component';
+import { ListadoClientesComponent } from './clientes/listado-clientes/listado-clientes.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NavComponent,
+    ListadoProductosComponent,
+    ListadoClientesComponent,
   ],
   imports: [
     BrowserModule,
